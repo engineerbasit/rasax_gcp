eeb81cc36d96d513fb4696bffcec386e3692b807
Abdul Basit <101786218+engineerbasit@users.noreply.github.com>
2022-04-12 15:58:47 +0500

    first commit

---
+++ actions/actions.py
@@ -0,0 +1,34 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet, EventType
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_server"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        buttons=[
+            {"payload":'/buyer{"property_type":"buyer"}', "title":"Buyer"},
+            {"payload":'/seller{"property_type":"seller"}', "title":"Seller"},
+        ]
+        
+        # a=tracker.get_slot("property_type")
+        dispatcher.utter_message(text=f"Welcome Dear", buttons=buttons)
+
+        return []
